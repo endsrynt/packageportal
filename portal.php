fec17d083cd9d1943305a93f31ee38a2c2166f12
endsrynt <84342810+endsrynt@users.noreply.github.com>
2022-01-05 16:06:57 +0700

    Add files via upload

---
+++ portal.php
@@ -0,0 +1,18 @@
+<?php 
+
+echo "=========================\n";
+echo "AUTO SCAN PACKAGE PORTAL\n";
+echo "=========================\n";
+echo "\n";
+echo "EMAIL PACKAGE : yaelahakowkoa@gmail.com\n";
+echo "PASSWORD : anjayw12\n";
+echo "PROSES LOGIN..";
+echo "\n";
+echo "=========================\n";
+echo "WALLET KAMU : zil1alyxu3ayhf3zfn3d4ye4u5uu59uey0jfq69txx\n";
+echo "=========================\n";
+echo "\n";
+echo "MASUKAN LIST RESI : resifreshh.txt\n";
+echo "PROSES SCAN..";
+
+ ?>
